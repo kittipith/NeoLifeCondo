694887d817978b06657247f669e4b147c66b516f
BBoonZ <bosszaza0380@gmail.com>
2025-02-28 09:07:28 +0700

    Update newboard

--- routes/user-login.js
+++ routes/user-login.js
@@ -15,7 +15,10 @@ router.get("/user/room", (req, res) => {
     // const decoded = jwt.verify(token, REFRESH_SECRET);
     // console.log("Decoded Token:", decoded);
 
-    res.render('newboard', {data :'none'});
+    db.all("SELECT * FROM news WHERE status = 1", (err, data) => {
+        console.log(data);
+        res.render('newboard', { data: data });
+    });
 });
 
 router.get("/user/information", (req, res) => {
@@ -29,27 +32,27 @@ router.get("/user/information", (req, res) => {
                 JOIN room r ON b.room_id = r.room_id  
                 JOIN users u ON r.renter_id = u.user_id
                 WHERE u.user_id = ${user.id} and b.isPaid = 0;`, [], (err, bill_data) => {
-                db.get(`SELECT COUNT(DISTINCT b.bill_id) AS total 
+            db.get(`SELECT COUNT(DISTINCT b.bill_id) AS total 
                         FROM bill b
                         JOIN room r ON b.room_id = r.room_id  
                         JOIN users u ON r.renter_id = u.user_id
                         WHERE u.user_id = ${user.id} and b.isPaid = 0;`, [], (err, countData) => {
-                    res.render("userinfo", { data: data, bill_data: bill_data, total: countData.total });
-                });
+                res.render("userinfo", { data: data, bill_data: bill_data, total: countData.total });
+            });
         });
     });
 });
 
 router.post("/user/service", (req, res) => {
-    const { numroom, service, date, time, note, filebase64 } = req.body;
-    
-    console.log("📌 รับข้อมูล:", { numroom, service, date, time, note });
-    console.log("🖼️ รูปภาพ Base64:", filebase64.substring(0, 100) + "..."); // แสดงเฉพาะ 100 ตัวอักษรแรก
+    const { numroom, service, date, time, note, filebase641 } = req.body;
 
-    if (service === "01"){
+    // console.log("📌 รับข้อมูล:", { numroom, service, date, time, note });
+    // console.log("🖼️ รูปภาพ Base64:", filebase64.substring(0, 100) + "..."); // แสดงเฉพาะ 100 ตัวอักษรแรก
+
+    if (service === "01") {
         db.get("SELECT room_id FROM room WHERE room_number = ?", [numroom], (err, room_id) => {
             // console.log(room_id.room_id);
-            db.run("INSERT INTO servicesReq (service_id, room_id, info, date, time, pic) VALUES (?, ?, ?, ?, ?, ?)", 
+            db.run("INSERT INTO servicesReq (service_id, room_id, info, date, time, pic) VALUES (?, ?, ?, ?, ?, ?)",
                 [serviceId, room_id.room_id, note, date, time, filebase64], (err) => {
                     if (err) {
                         console.error("Error inserting service request:", err);
@@ -57,11 +60,11 @@ router.post("/user/service", (req, res) => {
                         console.log("Service request inserted successfully");
                     }
                 });
-            });
-    }else if (service === "02"){
+        });
+    } else if (service === "02") {
         db.get("SELECT room_id FROM room WHERE room_number = ?", [numroom], (err, room_id) => {
             // console.log(room_id.room_id);
-            db.run("INSERT INTO repairReq (room_id, info, date, time, pic) VALUES (?, ?, ?, ?, ?)", 
+            db.run("INSERT INTO repairReq (room_id, info, date, time, pic) VALUES (?, ?, ?, ?, ?)",
                 [room_id.room_id, note, date, time, filebase64], (err) => {
                     if (err) {
                         console.error("Error inserting repair request:", err);
@@ -69,11 +72,44 @@ router.post("/user/service", (req, res) => {
                         console.log("Repair request inserted successfully");
                     }
                 });
-            });
+        });
     }
 
     res.redirect("/user/room");
     // res.json({ success: true, message: "ส่งข้อมูลสำเร็จ!", data: { numroom, service, date, time, note, filebase64 } });
+});
+
+router.post("/user/contactstaff", (req, res) => {
+    const { numroom, cont, date, time, title, info, filebase642 } = req.body;
+
+    // console.log("📌 รับข้อมูล:", { numroom, cont, date, time, title, info });
+    // console.log("🖼️ รูปภาพ Base64:", filebase642.substring(0, 100) + "..."); // แสดงเฉพาะ 100 ตัวอักษรแรก
+
+    if (cont === "01") {
+        // console.log(room_id.room_id);
+        db.run("INSERT INTO news (new_name, info, date, time, pic) VALUES (?, ?, ?, ?, ?)",
+            [title, info, date, time, filebase642], (err) => {
+                if (err) {
+                    console.error("Error inserting new request:", err);
+                } else {
+                    console.log("New request inserted successfully");
+                }
+            });
+    } else if (cont === "02") {
+        db.get("SELECT room_id FROM room WHERE room_number = ?", [numroom], (err, room_id) => {
+            // console.log(room_id.room_id);
+            db.run("INSERT INTO contact_staff (room_id, contact_name, info, date, time, pic) VALUES (?, ?, ?, ?, ?, ?)",
+                [room_id.room_id, tile, info, date, time, filebase642], (err) => {
+                    if (err) {
+                        console.error("Error inserting contactStaff request:", err);
+                    } else {
+                        console.log("ContactStaff request inserted successfully");
+                    }
+                });
+        });
+    }
+
+    res.redirect("/user/room");
 });
 
 
